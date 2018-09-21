4f28e32a93119736446244c637d0bfbc2d5afaec
Mike McGowan <mike.mcgowan@leolearning.com>
2018-09-21 11:12:51 +0100

    Tweaks

--- src/main/scala/mikemcgowan/Application.scala
+++ src/main/scala/mikemcgowan/Application.scala
@@ -1,28 +1,32 @@
 package mikemcgowan
 
 import org.querki.jquery._
-import org.scalajs.dom.{Event, XMLHttpRequest}
+import org.scalajs.dom.XMLHttpRequest
 
 object Application {
+
+  type Matrix = Array[Array[String]]
 
   private val url =
     "https://raw.githubusercontent.com/mikemcgowan/memrise-scraper-scala/master/memrise_database.csv"
 
-  def main(args: Array[String]): Unit = {
-    $(() => setupUI())
-    downloadCsv()
-  }
+  def main(args: Array[String]): Unit =
+    $(() => init())
 
-  def setupUI(): Unit = {
+  def setStatus(t: String): Unit =
+    $("#status") text t
+
+  def init(): Unit = {
     $("""<div id="status"></div>""").appendTo($("body"))
     setStatus("Please wait while the CSV file is downloaded ...")
+    downloadCsv()
   }
 
   def downloadCsv(): Unit = {
     val xhr = new XMLHttpRequest()
     xhr.open("GET", url)
-    xhr.onload = (e: Event) =>
-      if (xhr.status == 200) {
+    xhr.onload = _ =>
+     if (xhr.status == 200) {
         setStatus("Please wait while the CSV file is parsed ...")
         val data = parseCsv(xhr.responseText)
         data foreach (d => println("%s means %s" format(d(0), d(1))))
@@ -33,11 +37,9 @@ object Application {
     xhr.send()
   }
 
-  def parseCsv(raw: String): Array[Array[String]] = {
+  def parseCsv(raw: String): Matrix = {
     val lines = raw split "\r\n"
     lines.tail map (_.split(','))
   }
-
-  def setStatus(t: String): Unit = $("#status") text t
 
 }
