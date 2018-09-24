7c512d9ef0624c04441d4f637a7d413bdc52eb99
Mike McGowan <mike.mcgowan@leolearning.com>
2018-09-24 09:43:18 +0100

    Removed silly use of flatMap in class Interval

--- src/main/scala/mikemcgowan/Interval.scala
+++ src/main/scala/mikemcgowan/Interval.scala
@@ -7,22 +7,14 @@ class Interval {
   private var interval: Option[Int] = None
 
   def set(f: () => Unit, timeout: Int): Unit = {
-    interval flatMap Interval.clear
+    clear()
     interval = Some(setInterval(f, timeout))
   }
 
   def clear(): Unit = {
-    interval flatMap Interval.clear
+    if (interval.isDefined)
+      clearInterval(interval.get)
     interval = None
-  }
-
-}
-
-object Interval {
-
-  private def clear(i: Int): Option[Int] = {
-    clearInterval(i)
-    None
   }
 
 }
